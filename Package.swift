f29867cada70b914c051fe49fee3e770a19f966d
Zachary Davy <zdavy@8thlight.com>
2015-12-22 22:51:20 +0000

    bump to 0.0.3

--- Package.swift
+++ Package.swift
@@ -2,6 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "SUVExamples",
-    dependencies: [.Package(url: "https://github.com/zachmokahn/SUV.git", Version(0,0,2))],
+    dependencies: [.Package(url: "https://github.com/zachmokahn/SUV.git", Version(0,0,3))],
     targets: [Target(name: "EchoServer")]
 )
